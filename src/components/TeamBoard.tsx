4577af4aad8de6c230e5becf267f3de6296e5f22
Rafael Aparecido José dos Santos <rafael.franco@energisaterceiros.com.br>
2025-01-15 15:14:38 -0300

    arrumando o filtro das equipes

--- src/components/TeamBoard.tsx
+++ src/components/TeamBoard.tsx
@@ -54,7 +54,7 @@ export function TeamBoard({ teamMembersProps }: TeamBoardProps) {
   const [showSummary, setShowSummary] = useState(false);
 
   const filteredMembers = teamMembers.filter(member => {
-    const matchesTeam = selectedTeam ? member.lastTeam === selectedTeam : true;
+    const matchesTeam = selectedTeam ? member.currentTeam === selectedTeam : true;
     const matchesSearch = member.name.toLowerCase().includes(searchTerm.toLowerCase());
     return matchesTeam && matchesSearch;
   });
@@ -68,10 +68,8 @@ export function TeamBoard({ teamMembersProps }: TeamBoardProps) {
   };
 
   const assignTeamAutomatically = () => {
-    const validTeams = availableTeams.filter(
-      team => !['Pilotos', 'Circulo Verde', 'Circulo Vermelho', 'Circulo Amarelo', 
-        'Circulo Azul', 'Circulo Roxo','Sala'].includes(team.toLowerCase())
-    );
+    const validTeams = [ 'Alegria', 'Animação', 'Compras', 'Café/Cozinha', 'Liturgia', 
+  'Secretaria', 'Trânsito', 'Visitação', 'Ordem/Patrimonio']
     let teamAssignmentCountAux = 0;
     const auxTeamMembers = teamMembers.map(member => {
       if (member.currentTeam) return member; 
@@ -110,9 +108,9 @@ export function TeamBoard({ teamMembersProps }: TeamBoardProps) {
     // Criando uma aba para cada equipe
     Object.entries(groupedByTeam).forEach(([teamName, members]) => {
       const sheetData = [
-        ['ID', 'Nome', 'Idade', 'Celular', 'Posição', 'Última Equipe'], // Cabeçalho
+        ['foto', 'Nome', 'Idade', 'Celular', 'Posição', 'Última Equipe'], // Cabeçalho
         ...members.map(member => [
-          member.id,
+          member.photoUrl || "N/A",
           member.name,
           member.age || 'N/A',
           member.cel || 'N/A',
@@ -175,7 +173,7 @@ export function TeamBoard({ teamMembersProps }: TeamBoardProps) {
         setSearchTerm={setSearchTerm}
         selectedTeam={selectedTeam}
         setSelectedTeam={setSelectedTeam}
-        uniqueTeams={[]}
+        uniqueTeams={availableTeams}
       />
 
       <div className="md:grid sm:flex sm:flex-col sm:flex-wrap sm:items-center md:grid-cols-3 flex flex-wrap justify-between gap-3">
